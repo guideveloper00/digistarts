fc70be333d33a177a71c3eccd280c5ec64bad98d
Guilherme <guilherm3goncalves@hotmail.com>
2023-05-11 11:47:02 -0300

    fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const sequelize = require("./utils/database");
 const authRoutes = require("./routes/auth");
+const mysql = require('mysql');
 const userRoutes = require("./routes/user");
 const session = require("express-session");
 const bodyParser = require("body-parser");
@@ -11,11 +12,25 @@ const cors = require("cors");
 require('./swagger')(app);
 
 require("dotenv").config();
-const secretEnv = process.env.SECRET;
+const secretEnv = process.env.DB_PASSWORD;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '2035',
+  database: 'digistarts'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to MySQL database');
+});
 
 app.use(
   session({
